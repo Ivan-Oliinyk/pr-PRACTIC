e1219b3fd5087a1b7c61856e5e12c8e8f9cf0f89
Ivan <lorf1991@gmail.com>
2021-12-08 11:35:38 +0200

    add

--- prWebpack/src/index.js
+++ prWebpack/src/index.js
@@ -1,1 +1,15 @@
+import "./scss/common.scss";
+import "./scss/main.scss";
+
 console.log("sdasdassadd2");
+console.log(2);
+
+// создаем элемент заголовка
+const heading = document.createElement("h1");
+heading.textContent = "Как интересно 223!";
+
+// добавляем заголовок в DOM
+const root = document.querySelector("#root");
+root.append(heading);
+
+console.log([1, 2, 3].map((n) => n + 1));
--- prWebpack/webpack.config.js
+++ prWebpack/webpack.config.js
@@ -1,4 +1,6 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
   entry: "./src/index.js",
@@ -7,14 +9,49 @@ module.exports = {
     path: path.resolve(__dirname, "dist"),
   },
 
+  module: {
+    rules: [
+      {
+        test: /\.scss$/i,
+        use: [
+          "style-loader",
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          "sass-loader",
+        ],
+      },
+
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"],
+      },
+    ],
+  },
+
+  stats: "errors-only",
+  // mode: "development",
   devtool: "source-map",
 
   devServer: {
     port: 9001,
-    // open: true,
+    open: true,
+    // liveReload: true,
 
     static: {
-      directory: __dirname,
+      directory: path.resolve(__dirname, "dist"),
     },
   },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "webpack Boilerplate",
+      template: path.resolve(__dirname, "/src/template.html"), // шаблон
+      filename: "index.html", // название выходного файла
+      inject: "body",
+    }),
+    new MiniCssExtractPlugin({
+      filename: "style.css",
+    }),
+  ],
 };
