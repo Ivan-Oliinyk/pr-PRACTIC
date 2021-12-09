e4fcee2b82baabbbcec3f191a5dd2c05adb57d7e
Ivan <lorf1991@gmail.com>
2021-12-09 17:45:57 +0200

    qwe

--- Autorization-ulbtv/authController.js
+++ Autorization-ulbtv/authController.js
@@ -1,8 +1,18 @@
 const User = require("./models/User.js");
 const Role = require("./models/Role.js");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 const { validationResult } = require("express-validator");
+const { secret } = require("./config");
 
+const generateAccessToken = (id, roles) => {
+  const payload = {
+    id,
+    roles,
+  };
+
+  return kwt.sign(payload, secret, { expiresIn: "24h" });
+};
 class authController {
   async registration(req, res) {
     try {
@@ -37,9 +47,24 @@ class authController {
       console.log(e);
       res.status(400).json({ message: "Registration error" });
     }
+    const token = generateAccessToken(user._id, user.roles);
+    return res.json({ token });
   }
   async login(req, res) {
     try {
+      const { username, password } = req.body;
+      const user = await User.findOne({ username });
+
+      if (!user) {
+        return res
+          .status(400)
+          .json({ message: `User ${username} not found !` });
+      }
+
+      const validPassword = bcrypt.compareSync(password, user.password);
+      if (!validPassword) {
+        return res.status(400).json({ message: "Password is NOT TRUE" });
+      }
     } catch (e) {
       console.log(e);
       res.status(400).json({ message: "Loggin error" });
