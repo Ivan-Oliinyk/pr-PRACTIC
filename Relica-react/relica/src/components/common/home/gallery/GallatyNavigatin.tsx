71c84fffa657a7854d05c7f91e5d05f9c5db9301
Ivan <lorf1991@gmail.com>
2021-12-23 17:35:58 +0200

    gallary complited

---
+++ Relica-react/relica/src/components/common/home/gallery/GallatyNavigatin.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export const GallatyNavigatin: React.FC = () => {
+  const listContent = ["Most recent", "Most liked", "Discovered"];
+
+  return (
+    <nav className="chat-4__nav">
+      <ul className="nav__list">
+        {listContent.map((el) => (
+          <li className="nav__item">{el}</li>
+        ))}
+      </ul>
+    </nav>
+  );
+};
