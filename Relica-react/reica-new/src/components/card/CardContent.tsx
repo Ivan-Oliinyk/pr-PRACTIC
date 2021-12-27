d48f7c1c8dec4ea9ff962a7267ffede6606e6edf
Ivan <lorf1991@gmail.com>
2021-12-27 23:59:51 +0200

    home change

--- Relica-react/reica-new/src/components/card/CardContent.tsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export const CardContent: React.FC = () => {
-  return <></>;
-};
--- Relica-react/reica-new/src/components/layout/login/WelcomePage.tsx
+++ Relica-react/reica-new/src/components/layout/login/WelcomePage.tsx
@@ -1,9 +1,17 @@
 import React from "react";
-// import { Typography } from "../../typography/Typography";
-// import { TagVariants } from "../../typography/Typography";
-// import { TypographyProps } from "../../typography/Typography";
-import { Card } from "../../card/Card";
+import { Typography } from "../../typography/Typography";
+import { TagVariants } from "../../typography/Typography";
+import { TypographyProps } from "../../typography/Typography";
+// import { Card } from "../../card/Card";
 
 export const WelcomePage: React.FC = () => {
-  return <>{/* <Card /> */}</>;
+  return (
+    <>
+      <Typography
+        classname="h2 big"
+        tagname={TagVariants.h2}
+        children="Welcome"
+      />
+    </>
+  );
 };
