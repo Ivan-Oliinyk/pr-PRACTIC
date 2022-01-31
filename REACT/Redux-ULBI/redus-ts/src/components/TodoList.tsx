113c264a8e3ab4f64e7ea27dd45a6b46e7a8dd91
Ivan <lorf1991@gmail.com>
2022-01-31 19:01:42 +0200

    asw

--- REACT/Redux-ULBI/redus-ts/src/components/TodoList.tsx
+++ REACT/Redux-ULBI/redus-ts/src/components/TodoList.tsx
@@ -1,7 +1,6 @@
 import React, { useEffect } from "react";
 import { useActions } from "../hooks/useActions";
 import { useTypedSelector } from "../hooks/useTypeSelector";
-import { fetchTodos } from "../store/action-creators/todo";
 
 const TodoList: React.FC = () => {
   const { page, error, loading, limit, todos } = useTypedSelector(
--- REACT/Redux-ULBI/redus-ts/src/components/UserList.tsx
+++ REACT/Redux-ULBI/redus-ts/src/components/UserList.tsx
@@ -1,14 +1,11 @@
 import React, { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
 import { useActions } from "../hooks/useActions";
 import { useTypedSelector } from "../hooks/useTypeSelector";
-import { fetchUsers } from "../store/action-creators/user";
 
 const UserList: React.FC = () => {
   const { users, error, loading } = useTypedSelector((state) => state.user);
 
   const { fetchUsers } = useActions();
-  // const dispatch = useDispatch();
 
   useEffect(() => {
     fetchUsers();
